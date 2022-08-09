bd00538c592dade3898e3a2a73386402f9b06569
tuppercm <103149639+tuppercm@users.noreply.github.com>
2022-08-09 09:32:04 -0400

    Created initial application and added bootstrap

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <div className="btn btn-primary">Hello</div>
+    </div>
+  );
+}

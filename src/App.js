25f941a279f704f1f631dfdce729c3a88a1604a9
tuppercm <103149639+tuppercm@users.noreply.github.com>
2022-08-09 09:43:11 -0400

    Added footer content

--- src/App.js
+++ src/App.js
@@ -3,8 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
-      <div className="btn btn-primary">Hello</div>
+      <h1>Dictionary</h1>
+      <footer>
+        Coded by Caitlin Tupper,{" "}
+        <a
+          href="https://github.com/tuppercm/dictionary-app-react"
+          title="GitHub repository"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://vermillion-cupcake-044708.netlify.app/"
+          title="Netlify"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }

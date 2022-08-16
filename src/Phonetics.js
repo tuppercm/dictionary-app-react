deeddd8526d5116efd2b87b91f048b5cae53b922
tuppercm <103149639+tuppercm@users.noreply.github.com>
2022-08-16 08:30:49 -0400

    Improved Phonetics return
    Added IF statements to only return text and audio when present

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  return (
-    <div className="Phonetics">
-      {props.phonetics.text}
-      <br />
-      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-    </div>
-  );
+  if (props.phonetics.text && props.phonetics.audio) {
+    return (
+      <div className="Phonetics">
+        {props.phonetics.text}:{" "}
+        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+      </div>
+    );
+  } else if (props.phonetics.text) {
+    return <div className="Phonetics">{props.phonetics.text}</div>;
+  } else if (props.phonetics.audio) {
+    return (
+      <div className="Phonetics">
+        {" "}
+        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
